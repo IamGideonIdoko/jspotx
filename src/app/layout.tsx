ef07101db58e5ae501118369b31276887865ddad
Gideon Idoko <iamgideonidoko@gmail.com>
2023-08-13 05:07:49 +0100

    chore: set up linting and formatter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,22 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '@/styles/globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import '@/styles/globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 import SSRProvider from 'react-bootstrap/SSRProvider';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Jspotx',
   description: 'Job board with spotlight feature',
-}
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <SSRProvider>
       <html lang="en">
         <body className={inter.className}>{children}</body>
       </html>
     </SSRProvider>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import styles from '@/styles/home.module.css'
-import { Button } from '@/composables/bootstrap';
+import styles from '@/styles/home.module.css';
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Test App</h1>
-      <Button>Button</Button>
+      <h1>App</h1>
     </main>
-  )
+  );
 }
